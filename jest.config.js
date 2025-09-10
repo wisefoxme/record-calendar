e2deecb23597e772e7247abffbcf9cf025aa0514
Renato Oliveira <renato.oliveira@dogeforce.com>
2025-09-10 10:22:46 -0300

    ci: tries adding the module name wrapper to the top

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,13 @@
 const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 
 module.exports = {
+  moduleNameMapper: {
+    "^c/(.*)$": "<rootDir>/force-app/main/default/lwc/$1/$1"
+  },
   ...jestConfig,
   modulePathIgnorePatterns: [
     "<rootDir>/.localdevserver",
     "<rootDir>/node_modules",
     "<rootDir>/force-app/wisefoxme"
-  ],
-  moduleNameMapper: {
-    "^c/(.*)$": "<rootDir>/force-app/main/default/lwc/$1/$1"
-  }
+  ]
 };
